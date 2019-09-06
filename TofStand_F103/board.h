3c0e463debbf6dedf5545a81d049ea138659f2f7
Kreyl <laystoll@yandex.ru>
2019-09-07 00:06:42 +0300

    + F103 + UART works

---
+++ TofStand_F103/board.h
@@ -0,0 +1,140 @@
+/*
+ * board.h
+ *
+ *  Created on: 12 сент. 2015 г.
+ *      Author: Kreyl
+ */
+
+#pragma once
+
+// ==== General ====
+#define BOARD_NAME          "MotorCtrl01"
+#define APP_NAME            "TofStandF103"
+
+// MCU type as defined in the ST header.
+#define STM32F103xE
+
+// Freq of external crystal if any. Leave it here even if not used.
+#define CRYSTAL_FREQ_HZ         12000000
+
+// OS timer settings
+#define STM32_ST_IRQ_PRIORITY   2
+#define STM32_ST_USE_TIMER      2
+#define SYS_TIM_CLK             (Clk.GetTimInputFreq(TIM2))
+
+//  Periphery
+#define I2C1_ENABLED            FALSE
+#define I2C2_ENABLED            FALSE
+#define I2C3_ENABLED            FALSE
+#define SIMPLESENSORS_ENABLED   FALSE
+#define BUTTONS_ENABLED         FALSE
+
+#define ADC_REQUIRED            FALSE
+
+#if 1 // ========================== GPIO =======================================
+// EXTI
+#define INDIVIDUAL_EXTI_IRQ_REQUIRED    FALSE
+
+// UART
+#define UART_GPIO       GPIOB
+#define UART_TX_PIN     6
+#define UART_RX_PIN     7
+
+// USB
+#define USB_DETECT_PIN  GPIOA, 9
+#define USB_DM          GPIOA, 11
+#define USB_DP          GPIOA, 12
+#define USB_AF          AF10
+#define BOARD_OTG_NOVBUSSENS    TRUE
+
+// Endstops
+#define ENDSTOP1        GPIOC, 11
+#define ENDSTOP2        GPIOC, 12
+
+#if 1 // ==== L6470 ====
+#define M_SPI           SPI1
+#define M_SPI_GPIO      GPIOA
+#define M_SCK           5
+#define M_MISO          6
+#define M_MOSI          7
+#define M_CS            8
+#define M_SPI_AF        AF5
+
+#define M_AUX_GPIO      GPIOC
+#define M_BUSY_SYNC1    0
+#define M_SW1           1
+#define M_FLAG1         2
+#define M_STBY_RST      6
+#endif
+
+#endif // GPIO
+
+#if I2C2_ENABLED // ====================== I2C ================================
+#define I2C2_BAUDRATE   400000
+#define I2C_PILL        i2c2
+#endif
+
+#if ADC_REQUIRED // ======================= Inner ADC ==========================
+#define ADC_MEAS_PERIOD_MS  2007
+// Clock divider: clock is generated from the APB2
+#define ADC_CLK_DIVIDER		adcDiv4
+
+// ADC channels
+#define ADC_CHNL_BATTERY    10
+
+#define ADC_CHANNELS        { ADC_CHNL_BATTERY, ADC_CHNL_VREFINT }
+#define ADC_CHANNEL_CNT     2   // Do not use countof(AdcChannels) as preprocessor does not know what is countof => cannot check
+#define ADC_SAMPLE_TIME     ast84Cycles
+#define ADC_SAMPLE_CNT      8   // How many times to measure every channel
+
+#define ADC_MAX_SEQ_LEN     16  // 1...16; Const, see ref man
+#define ADC_SEQ_LEN         (ADC_SAMPLE_CNT * ADC_CHANNEL_CNT)
+#if (ADC_SEQ_LEN > ADC_MAX_SEQ_LEN) || (ADC_SEQ_LEN == 0)
+#error "Wrong ADC channel count and sample count"
+#endif
+#endif
+
+#if 1 // =========================== DMA =======================================
+#define STM32_DMA_REQUIRED  TRUE
+// ==== Uart ====
+// Remap is made automatically if required
+#define UART_DMA_TX     STM32_DMA_STREAM_ID(1, 4)
+#define UART_DMA_RX     STM32_DMA_STREAM_ID(1, 5)
+#define UART_DMA_CHNL   1 // dummy
+#define UART_DMA_TX_MODE(Chnl) (STM32_DMA_CR_CHSEL(Chnl) | DMA_PRIORITY_LOW | STM32_DMA_CR_MSIZE_BYTE | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MINC | STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_TCIE)
+#define UART_DMA_RX_MODE(Chnl) (STM32_DMA_CR_CHSEL(Chnl) | DMA_PRIORITY_MEDIUM | STM32_DMA_CR_MSIZE_BYTE | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MINC | STM32_DMA_CR_DIR_P2M | STM32_DMA_CR_CIRC)
+
+#if I2C1_ENABLED // ==== I2C1 ====
+#define I2C1_DMA_TX     STM32_DMA1_STREAM6
+#define I2C1_DMA_RX     STM32_DMA1_STREAM5
+#endif
+#if I2C2_ENABLED // ==== I2C2 ====
+#define I2C2_DMA_TX     STM32_DMA1_STREAM7
+#define I2C2_DMA_RX     STM32_DMA1_STREAM3
+#endif
+
+#if ADC_REQUIRED
+#define ADC_DMA         STM32_DMA2_STREAM0
+#define ADC_DMA_MODE    STM32_DMA_CR_CHSEL(0) |   /* DMA2 Stream0 Channel0 */ \
+                        DMA_PRIORITY_LOW | \
+                        STM32_DMA_CR_MSIZE_HWORD | \
+                        STM32_DMA_CR_PSIZE_HWORD | \
+                        STM32_DMA_CR_MINC |       /* Memory pointer increase */ \
+                        STM32_DMA_CR_DIR_P2M |    /* Direction is peripheral to memory */ \
+                        STM32_DMA_CR_TCIE         /* Enable Transmission Complete IRQ */
+#endif // ADC
+
+#endif // DMA
+
+#if 1 // ========================== USART ======================================
+#define PRINTF_FLOAT_EN FALSE
+#define UART_TXBUF_SZ   8192
+#define UART_RXBUF_SZ   99
+
+#define UARTS_CNT       1
+
+#define CMD_UART_PARAMS \
+    USART1, UART_GPIO, UART_TX_PIN, UART_GPIO, UART_RX_PIN, \
+    UART_DMA_TX, UART_DMA_RX, UART_DMA_TX_MODE(UART_DMA_CHNL), UART_DMA_RX_MODE(UART_DMA_CHNL)
+
+#endif
---
+++ TofStand_F103/main.cpp
@@ -0,0 +1,230 @@
+#include "hal.h"
+#include "MsgQ.h"
+#include "kl_lib.h"
+//#include "Sequences.h"
+#include "uart.h"
+#include "shell.h"
+//#include "usb_cdc.h"
+//#include "L6470.h"
+//#include "led.h"
+
+#if 1 // =============== Low level ================
+// Forever
+EvtMsgQ_t<EvtMsg_t, MAIN_EVT_Q_LEN> EvtQMain;
+static const UartParams_t CmdUartParams(115200, CMD_UART_PARAMS);
+CmdUart_t Uart{&CmdUartParams};
+static void ITask();
+static void OnCmd(Shell_t *PShell);
+
+//static bool UsbPinWasHi = false;
+static TmrKL_t TmrOneSecond {TIME_MS2I(999), evtIdEverySecond, tktPeriodic};
+
+//L6470_t Motor{M_SPI};
+
+//#define STEPS_IN_STAND  450000
+
+//LedBlinker_t Led(GPIOB, 2, omPushPull);
+
+//void EndstopHandler();
+//PinIrq_t EndstopTop{ENDSTOP2, pudPullDown, EndstopHandler};
+//PinIrq_t EndstopBottom{ENDSTOP1, pudPullDown, EndstopHandler};
+
+//void BusyHandler();
+//PinIrq_t Busy{M_AUX_GPIO, M_BUSY_SYNC1, pudPullUp, BusyHandler};
+#endif
+
+int main() {
+    // ==== Setup clock ====
+//    Clk.SetCoreClk(cclk48MHz);
+    Clk.UpdateFreqValues();
+
+    // ==== Init OS ====
+    halInit();
+    chSysInit();
+
+//    Led.Init();
+//    Led.On();
+
+    // ==== Init Hard & Soft ====
+//    JtagDisable();
+    EvtQMain.Init();
+    Uart.Init();
+    AFIO->MAPR |= AFIO_MAPR_USART1_REMAP; // Remap UART to PB6/PB7 pins
+    Printf("\r%S %S\r\n", APP_NAME, XSTRINGIFY(BUILD_TIME));
+    Clk.PrintFreqs();
+
+//    UsbCDC.Init();
+//    PinSetupInput(USB_DETECT_PIN, pudPullDown); // Usb detect pin
+
+    // Motor
+//    Motor.Init();
+//    Motor.SetAcceleration(1200);
+//    Motor.SetDeceleration(12000);
+//    Motor.SetStepMode(sm128);
+//    // Current
+//    Motor.SetCurrent4Run(18);
+//    Motor.SetCurrent4Hold(18);
+
+    // Endstops
+//    EndstopTop.Init(ttRising);
+//    EndstopBottom.Init(ttRising);
+//    EndstopTop.EnableIrq(IRQ_PRIO_MEDIUM);
+//    // Busy
+//    Busy.Init(ttRising);
+//    Busy.EnableIrq(IRQ_PRIO_MEDIUM);
+
+//    chThdSleepMilliseconds(720); // Let power to stabilize
+    // Go top if not yet
+//    if(!EndstopTop.IsHi()) Motor.Move(dirForward, 54000, STEPS_IN_STAND);
+
+//    TmrOneSecond.StartOrRestart();
+
+//    Led.Off();
+
+    // ==== Main cycle ====
+    ITask();
+}
+
+__noreturn
+void ITask() {
+    while(true) {
+        EvtMsg_t Msg = EvtQMain.Fetch(TIME_INFINITE);
+//        Printf("Msg.ID %u\r", Msg.ID);
+        switch(Msg.ID) {
+            case evtIdShellCmd:
+//                Led.StartOrContinue(lsqCmd);
+                OnCmd((Shell_t*)Msg.Ptr);
+                ((Shell_t*)Msg.Ptr)->SignalCmdProcessed();
+                break;
+
+            case evtIdEverySecond:
+//                Printf("Second\r");
+                // Check if USB connected/disconnected
+//                if(PinIsHi(USB_DETECT_PIN) and !UsbPinWasHi) {
+//                    UsbPinWasHi = true;
+//                    EvtQMain.SendNowOrExit(EvtMsg_t(evtIdUsbConnect));
+//                }
+//                else if(!PinIsHi(USB_DETECT_PIN) and UsbPinWasHi) {
+//                    UsbPinWasHi = false;
+//                    EvtQMain.SendNowOrExit(EvtMsg_t(evtIdUsbDisconnect));
+//                }
+                break;
+
+//            case evtIdBusyFlagHi:
+//                PrintfI("Ready\r\n");
+//                if(UsbCDC.IsActive()) UsbCDC.Print("Ready\r\n");
+//                break;
+
+#if 0 // ======= USB =======
+            case evtIdUsbConnect:
+//                Printf("USB connect\r");
+                UsbCDC.Connect();
+                break;
+            case evtIdUsbDisconnect:
+                UsbCDC.Disconnect();
+//                Printf("USB disconnect\r");
+                break;
+            case evtIdUsbReady:
+//                Printf("USB ready\r");
+                break;
+#endif
+
+            default: break;
+        } // switch
+    } // while true
+}
+
+
+//void EndstopHandler() {
+////    Motor.SwitchLoHi(); // HardStop the motor using switch. Way faster than SPI cmd.
+//}
+
+//void BusyHandler() {
+////    PrintfI("BusyHandler\r");
+//    EvtQMain.SendNowOrExitI(EvtMsg_t(evtIdBusyFlagHi));
+//}
+
+#if 1 // ======================= Command processing ============================
+void OnCmd(Shell_t *PShell) {
+    Cmd_t *PCmd = &PShell->Cmd;
+//    Printf("%S  ", PCmd->Name);
+
+    // Handle command
+    if(PCmd->NameIs("Ping")) PShell->Ack(retvOk);
+    else if(PCmd->NameIs("Version")) PShell->Print("%S %S\r\n", APP_NAME, XSTRINGIFY(BUILD_TIME));
+
+//    else if(PCmd->NameIs("Down")) {
+//        // Check if top endstop reched
+//        if(EndstopBottom.IsHi()) { PShell->Ack(retvBadState); return; }
+//        uint32_t ISteps = STEPS_IN_STAND, ISpd = SPD_MAX;
+//        PCmd->GetNext<uint32_t>(&ISpd); // May absent
+//        PCmd->GetNext<uint32_t>(&ISteps); // May absent
+//        if(ISteps == 0) { PShell->Ack(retvBadValue); return; }
+//        if(ISpd == 0) { PShell->Ack(retvBadValue); return; }
+//        if(ISpd > SPD_MAX) { PShell->Ack(retvBadValue); return; }
+//        Motor.Move(dirReverse, ISpd, ISteps);
+//        PShell->Ack(retvOk);
+//    }
+//    else if(PCmd->NameIs("Up")) {
+//        // Check if top endstop reched
+//        if(EndstopTop.IsHi()) { PShell->Ack(retvBadState); return; }
+//        uint32_t ISteps = STEPS_IN_STAND, ISpd = SPD_MAX;
+//        PCmd->GetNext<uint32_t>(&ISpd); // May absent
+//        PCmd->GetNext<uint32_t>(&ISteps); // May absent
+//        if(ISteps == 0) { PShell->Ack(retvBadValue); return; }
+//        if(ISpd == 0) { PShell->Ack(retvBadValue); return; }
+//        if(ISpd > SPD_MAX) { PShell->Ack(retvBadValue); return; }
+//        Motor.Move(dirForward, ISpd, ISteps);
+//        PShell->Ack(retvOk);
+//    }
+//
+//    else if(PCmd->NameIs("Stop")) {
+//        Motor.StopSoftAndHold();
+//        PShell->Ack(retvOk);
+//    }
+
+#if 0 // ==== Read/Write regs ====
+    else if(PCmd->NameIs("Get8")) {
+        uint8_t Addr = 0;
+        if(PCmd->GetNext<uint8_t>(&Addr) != retvOk) { PShell->Ack(retvCmdError); return; }
+        uint32_t Value = Motor.GetParam8(Addr);
+        Printf("%u  0x%X\r", Value, Value);
+    }
+    else if(PCmd->NameIs("Get16")) {
+        uint8_t Addr = 0;
+        if(PCmd->GetNext<uint8_t>(&Addr) != retvOk) { PShell->Ack(retvCmdError); return; }
+        uint32_t Value = Motor.GetParam16(Addr);
+        Printf("%u  0x%X\r", Value, Value);
+    }
+    else if(PCmd->NameIs("Get32")) {
+        uint8_t Addr = 0;
+        if(PCmd->GetNext<uint8_t>(&Addr) != retvOk) { PShell->Ack(retvCmdError); return; }
+        uint32_t Value = Motor.GetParam32(Addr);
+        Printf("%u  0x%X\r", Value, Value);
+    }
+
+    else if(PCmd->NameIs("Set8")) {
+        uint8_t Addr = 0;
+        uint32_t Value;
+        if(PCmd->GetNext<uint8_t>(&Addr) != retvOk) { PShell->Ack(retvCmdError); return; }
+        if(PCmd->GetNext<uint32_t>(&Value) != retvOk) { PShell->Ack(retvCmdError); return; }
+        Motor.SetParam8(Addr, Value);
+        PShell->Ack(retvOk);
+    }
+    else if(PCmd->NameIs("Set16")) {
+        uint8_t Addr = 0;
+        uint32_t Value;
+        if(PCmd->GetNext<uint8_t>(&Addr) != retvOk) { PShell->Ack(retvCmdError); return; }
+        if(PCmd->GetNext<uint32_t>(&Value) != retvOk) { PShell->Ack(retvCmdError); return; }
+        Motor.SetParam16(Addr, Value);
+        PShell->Ack(retvOk);
+    }
+#endif
+
+    else {
+        Printf("%S\r\n", PCmd->Name);
+//        if(UsbCDC.IsActive()) UsbCDC.Print("%S\r\n", PCmd->Name);
+        PShell->Ack(retvCmdUnknown);
+    }
+}
+#endif
