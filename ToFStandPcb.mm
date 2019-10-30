f55fb345764194012032430fa4845c35d71d1265
kreyl <laystoll@yandex.ru>
2019-10-30 18:51:07 +0300

    + V2.0 Sch done

---
+++ ToFStandPcb.mm
@@ -0,0 +1,74 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="ToFStandPcb" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1571150654772"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode TEXT="OK">
+<icon BUILTIN="button_ok"/>
+</stylenode>
+<stylenode TEXT="NotOk">
+<icon BUILTIN="button_cancel"/>
+</stylenode>
+<stylenode TEXT="Question">
+<icon BUILTIN="help"/>
+</stylenode>
+<stylenode TEXT="Attention">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stopper">
+<icon BUILTIN="stop-sign"/>
+</stylenode>
+<stylenode TEXT="Waiting">
+<icon BUILTIN="hourglass"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" BACKGROUND_COLOR="#ccffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" BACKGROUND_COLOR="#ffffcc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" BACKGROUND_COLOR="#ffcccc"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Motor" POSITION="right" ID="ID_1041304780" CREATED="1571150671301" MODIFIED="1571150675519">
+<node TEXT="L6470" ID="ID_650806515" CREATED="1571150702471" MODIFIED="1571150706211">
+<node TEXT="&#x440;&#x430;&#x434;&#x438;&#x430;&#x442;&#x43e;&#x440;" ID="ID_146698365" CREATED="1571150706981" MODIFIED="1571150713588"/>
+</node>
+</node>
+<node TEXT="Power" POSITION="right" ID="ID_747807968" CREATED="1571150677080" MODIFIED="1571150679852">
+<node TEXT="20 to 5" FOLDED="true" ID="ID_1750610861" CREATED="1571150723557" MODIFIED="1571150729377">
+<node TEXT="LM3150" ID="ID_614506393" CREATED="1571150731053" MODIFIED="1571150735627"/>
+</node>
+<node TEXT="5 to 3v3" ID="ID_1460351165" CREATED="1571150770650" MODIFIED="1571150777157">
+<node TEXT="MCP1700" ID="ID_493335714" CREATED="1571150778241" MODIFIED="1571150787915"/>
+</node>
+</node>
+<node TEXT="MCU" POSITION="right" ID="ID_782830667" CREATED="1571150680481" MODIFIED="1571150682307"/>
+<node TEXT="Endstops" POSITION="right" ID="ID_846723713" CREATED="1571150683001" MODIFIED="1571150687996"/>
+<node TEXT="Connection" POSITION="left" ID="ID_17266460" CREATED="1571150807271" MODIFIED="1571150811768"/>
+</node>
+</map>
